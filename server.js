0eb98e491aae6a0767962e7d233b30aa16eee4ec
marifolsom <mfolsom1@fordham.edu>
2018-04-12 10:21:12 -0400

    added some boiler plate into server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+
+// Require models
+const Event = require('./model/Event');
+const User = require('./model/User');
+
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Enable cross-origin support so the site loaded on localhost:3000
+// Can make AJAX requests to the server on localhost:4567
+app.use(cors());
+
+// Create application/json parser
+const jsonParser = bodyParser.json();
+
+
+// Start server
+app.listen(4567, () => console.log('Express server listening on port 4567!'));
