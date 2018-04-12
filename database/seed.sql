601c745ceda691e8eaf27a82287803a288d1ea1d
Rafal Borynski <rafalborynski@ool-ad020107.dyn.optonline.net>
2018-04-12 11:41:16 -0400

    schema-n-seed-done

---
+++ database/seed.sql
@@ -0,0 +1,13 @@
+\c calendar_db
+INSERT INTO users
+  (user_id, username, password, session_id)
+VALUES
+            ('RBorg', 'hi27', 'supersession');
+
+ -- psql -f ./database/seed.sql
+INSERT INTO events
+  (event_id,user_id, event_name, event_time, event_description, event_type)
+VALUES
+                (1, 'RParty', 2012-04-01 00:00:00-00, 'byob', party);
+
+
