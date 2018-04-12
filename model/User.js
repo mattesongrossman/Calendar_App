5329246343354dfc616347f76c292bf697d0b304
marifolsom <mfolsom1@fordham.edu>
2018-04-12 10:13:47 -0400

    added create and find methods to user model

---
+++ model/User.js
@@ -0,0 +1,25 @@
+const db = require('../database/db-connection');
+
+const User = {};
+
+
+User.create = (newUsername, hashedPassword) => {
+  return db.one(`
+    INSERT INTO user_info (username, password)
+    VALUES ($1, $2)
+    RETURNING id`,
+    [newUsername, hashedPassword]
+  )
+}
+
+User.find = enteredUsername => {
+  return db.one(`
+    SELECT *
+    FROM user_info
+    WHERE username = $1`,
+    [enteredUsername]
+  )
+}
+
+
+module.exports = User;
