f0fb95ff49a35809668e82d7a876e4ab4206a69b
marifolsom <mfolsom1@fordham.edu>
2018-04-12 10:50:38 -0400

    started psuedocoging routes for server.js

--- server.js
+++ server.js
@@ -18,5 +18,27 @@ app.use(cors());
 const jsonParser = bodyParser.json();
 
 
+// Get one user
+
+
+// Create new user
+
+
+// Get all events for one user
+
+
+// Get one event from one user's events
+
+
+// Create an event
+
+
+// Edit an event
+
+
+// Delete an event
+
+
+
 // Start server
 app.listen(4567, () => console.log('Express server listening on port 4567!'));
