1f6f2d9d8b32cc230f6d7100ea5ceeb4009fa772
marifolsom <mfolsom1@fordham.edu>
2018-04-13 16:22:45 -0400

    uncommented login, register, navbar

--- calendar_app/src/NavBar/index.js
+++ calendar_app/src/NavBar/index.js
@@ -1,18 +1,18 @@
-// import import React, { Component } from "react"
-// import Login from "../Login"
-// import AddEvent from "../AddEvent"
-// import { BrowserRouter as Router, Route, Link } from "react-router-dom"
-//
-//
-// //export default Navbar;
-// class Register extends Component {
-//  constructor(props) {
-//     super(props)
-//     this.state = {
-//       render{
-//   <h1> Calendar App </h1>
-//  < Route Path="/event/AddEvent"> component={AddEvent} />
-// 
-//
-// }
-// export default NavBar
+import import React, { Component } from "react"
+import Login from "../Login"
+import AddEvent from "../AddEvent"
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+
+
+//export default Navbar;
+class Register extends Component {
+ constructor(props) {
+    super(props)
+    this.state = {
+      render{
+  <h1> Calendar App </h1>
+ < Route Path="/event/AddEvent"> component={AddEvent} />
+
+
+}
+export default NavBar
