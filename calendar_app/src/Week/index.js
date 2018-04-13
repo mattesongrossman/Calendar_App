ae2b75f5ea74cb27b988d30b6a73e42bb7130e07
marifolsom <mfolsom1@fordham.edu>
2018-04-13 14:29:49 -0400

    created a week component

---
+++ calendar_app/src/Week/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Day from '../Week';
+
+class Week extends Component {
+  render() {
+    return (
+      <div className="week">
+        <Day />
+      </div>
+    )
+  }
+}
+
+export default Week;
