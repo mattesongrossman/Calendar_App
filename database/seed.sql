34921969fa366b8f87c3bbbffdbfddefd1ac0e3b
marifolsom <mfolsom1@fordham.edu>
2018-04-13 11:34:55 -0400

    added more dummy data to work with

--- database/seed.sql
+++ database/seed.sql
@@ -8,5 +8,7 @@ VALUES
 INSERT INTO events
   (user_id, event_name, event_time, event_description, event_type)
 VALUES
-  (1, 'RParty', '2012-04-01 00:00:00-00', 'byob', 'party'),
-  (1, 'Project 3 due!', '2018-04-16 09:00:00-00', 'Everything for project 3 + presentations due', 'HW');
+  (1, 'RParty', '2012-04-01', 'byob', 'party'),
+  (1, 'Project 3 due!', '2018-04-16 09:00:00-00', 'Everything for project 3 and presentations due', 'HW'),
+  (1, 'Bagels', '2018-04-13', 'Matteson''s bringing in bagels!', 'Food'),
+  (1, 'Watch Isle of Dogs', '2017-05-13 18:00:00-00', 'Isle of Dogs at Cobble Hill Cinemas', 'Movie');
