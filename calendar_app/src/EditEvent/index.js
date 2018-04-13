c167020b86e96e0af3a7e7d396944788c2996cd5
Matteson Grossman <mattesongrossman@ool-ad020277.dyn.optonline.net>
2018-04-13 12:25:35 -0400

    EditEvent - Added states, componentDidMount, event listeners. Not able to pull params for ID

--- calendar_app/src/EditEvent/index.js
+++ calendar_app/src/EditEvent/index.js
@@ -9,22 +9,112 @@ import {
 class EditEvent extends Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      editEvent: {
+        id: null,
+        name: "",
+        time: "",
+        description: "",
+        type: ""
+      },
+      created: false,
+      edited: false
+    }
+    this.updateEvent = this.updateEvent.bind(this)
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.deleteEvent = this.deleteEvent.bind(this)
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    const id = Number(this.props.match.params.id)
+    console.log(id)
+    fetch(`http://localhost:4567/api/event/1`)
+      .then(response => response.json())
+      .then(event => {
+        const {
+          id,
+          event_name,
+          event_time,
+          event_description,
+          event_type
+        } = event
+        this.setState({
+          editEvent: {
+            name: event_name,
+            time: event_time,
+            description: event_description,
+            type: event_type
+          }
+        })
+      })
+  }
+
+  handleInputChange(evt) {
+    //selects the name of the input field
+    const property = evt.target.name
+    //selects the value of the input field
+    const value = evt.target.value
+    this.setState((prevState, props) => {
+      const editEvent = Object.assign({}, prevState.editEvent)
+      editEvent[property] = value
+      return {
+        editEvent: editEvent
+      }
+    })
+  }
+
+  updateEvent(evt) {
+    evt.preventDefault()
+
+    const { name, time, description, type } = this.state.editEvent
+    const id = this.props.match.params
+    const body = {
+      name: name,
+      time: time,
+      description: description,
+      type: type
+    }
+    fetch(`http://localhost:4567/api/record/1`, {
+      method: "PUT",
+      body: JSON.stringify(body),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }).then(response => {
+      this.setState({
+        edited: true
+      })
+    })
+  }
+
+  deleteEvent(evt) {
+    evt.preventDefault()
+    const id = this.props.match.params
+    fetch(`http://localhost:4567/api/event/1`, { method: "DELETE" })
+      .then(response => response.json())
+      .then(responseJSON => {
+        this.setState({
+          edited: true
+        })
+      })
+  }
 
   render() {
+    const { name, time, description, type } = this.state.editEvent
+
     return (
       <div>
         <h3>Edit</h3>
-        <form id="edit">
+        <form
+          id="edit"
+          onChange={this.handleInputChange}
+          onSubmit={this.updateEvent}>
           <div className="form-group">
             <label>
               Event Name:
               <input
                 type="text"
-                value=""
+                value={name}
                 className="form-control"
                 placeholder="Enter event name"
                 name="name"
@@ -37,7 +127,7 @@ class EditEvent extends Component {
               Date:
               <input
                 type="datetime-local"
-                value=""
+                value={time}
                 className="form-control"
                 placeholder="Enter time"
                 name="time"
@@ -47,16 +137,14 @@ class EditEvent extends Component {
           <div className="form-group">
             <label>Description: </label>
             <br />
-            <textarea name="edit" form="edit">
-              Enter text here...
-            </textarea>
+            <textarea name="edit" form="edit" value={description} />
           </div>
           <div className="form-group">
             <label>
               Type:
               <input
                 type="text"
-                value=""
+                value={type}
                 className="form-control"
                 placeholder="Enter type"
                 name="type"
@@ -70,7 +158,10 @@ class EditEvent extends Component {
           </div>
         </form>
         <form className="form-group">
-          <button type="submit" className="btn btn-danger">
+          <button
+            type="submit"
+            className="btn btn-danger"
+            onSubmit={this.deleteEvent}>
             Delete
           </button>
         </form>
