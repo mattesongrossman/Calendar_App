97ca5f367ddb6fc29ef440ff0e7d602bdeb48596
marifolsom <mfolsom1@fordham.edu>
2018-04-13 14:29:30 -0400

    commented out login, register, and navbar components

--- calendar_app/src/Register/index.js
+++ calendar_app/src/Register/index.js
@@ -1,73 +1,73 @@
-import import React, { Component } from "react"
-import Login from "../Login"
-import NavBar from "../NaVBar"
-import { BrowserRouter as Router, Route, Link } from "react-router-dom"
-
-class Register extends Component {
- constructor(props) {
-    super(props)
-    this.state = {
- <{NavBar}>
-<h1> Register as a New User! </h1>
-    username: '',
-      password: ''
-
-
-
-
-
-
-
-
-//       },
-      created: false
-    }
-    this.createUser = this.createUser.bind(this)
-
-  }
-
-  componentDidMount() {}
-
-  newUser(evt) {
-    //selects the name of the input field
-    const username = evt.target.name
-    //selects the value of the input field
-    const password = evt.target.value
-  };
-    this.setState((prevState, props) => {
-      const newEvent = Object.assign({}, prevState.newEvent)
-      NewUser[property] = value
-      return {
-        newUser: newUser
-      }
-    })
-  }
-
-
-
-
-  <form onSubmit={this.newUser}>
-        <label>
-          <input>
-            type="text"
-            name="username"
-            value={this.state.username} <input/>
-             <input>
-            type="text"
-            name="password"
-            value={this.state.password} <input/>
-
-
-        </label>
-
-
-        <button type="submit" value="Submit" onClick= onSubmit> Submit </button>
-      <h2>Already have an account? </h2>
- <button>< Login Route path= "/event/login" component={Login} /> </button>
-
-      </form>
-
-
-
-export default Register
-// https://git.generalassemb.ly/wdi-nyc-ewok/auth_react/blob/master/client/src/components/UserForm.js
+// import import React, { Component } from "react"
+// import Login from "../Login"
+// import NavBar from "../NaVBar"
+// import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+//
+// class Register extends Component {
+//  constructor(props) {
+//     super(props)
+//     this.state = {
+//  <{NavBar}>
+// <h1> Register as a New User! </h1>
+//     username: '',
+//       password: ''
+//
+//
+//
+//
+//
+//
+//
+//
+// //       },
+//       created: false
+//     }
+//     this.createUser = this.createUser.bind(this)
+//
+//   }
+//
+//   componentDidMount() {}
+//
+//   newUser(evt) {
+//     //selects the name of the input field
+//     const username = evt.target.name
+//     //selects the value of the input field
+//     const password = evt.target.value
+//   };
+//     this.setState((prevState, props) => {
+//       const newEvent = Object.assign({}, prevState.newEvent)
+//       NewUser[property] = value
+//       return {
+//         newUser: newUser
+//       }
+//     })
+//   }
+//
+//
+//
+//
+//   <form onSubmit={this.newUser}>
+//         <label>
+//           <input>
+//             type="text"
+//             name="username"
+//             value={this.state.username} <input/>
+//              <input>
+//             type="text"
+//             name="password"
+//             value={this.state.password} <input/>
+//
+//
+//         </label>
+//
+//
+//         <button type="submit" value="Submit" onClick= onSubmit> Submit </button>
+//       <h2>Already have an account? </h2>
+//  <button>< Login Route path= "/event/login" component={Login} /> </button>
+//
+//       </form>
+//
+//
+//
+// export default Register
+// // https://git.generalassemb.ly/wdi-nyc-ewok/auth_react/blob/master/client/src/components/UserForm.js
