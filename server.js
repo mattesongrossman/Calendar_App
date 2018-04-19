d990893866d9933dd5909145df64e3419744df0b
marifolsom <mfolsom1@fordham.edu>
2018-04-19 15:42:21 -0400

    required path in server.js and created procfile

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require("cors")
 const bodyParser = require("body-parser")
 const app = express()
 const tokenService = require("./services/TokenService")
+const path = require('path');
 const PORT = process.env.PORT || 4567
 
 // Require models
